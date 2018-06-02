2896ef5f261253bf5e84d93d0eaff7dcd3178ee4
Shelita Soriano <sprinklecoded@gmail.com>
2018-06-02 15:04:09 -0500

    Adding assignment1.4 Soriano-assignment1.4.js

---
+++ week-1/Soriano-assignment1.4.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: June 2, 2018
+; Modified By: Shelita Soriano
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+
+//Assignment 1.4 - Primitive Types
+
+var firstName ="Shelita";
+var lastName = "Soriano";
+var age = "33";
+var street = "First St.";
+var city = "Classico City";
+var state = "California";
+var zipCode ="54321";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
