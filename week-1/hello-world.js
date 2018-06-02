22d9875f04295255d72e934f8bdca1294e40aafa
Shelita Soriano <sprinklecoded@gmail.com>
2018-06-02 12:04:20 -0500

    Adding assignment 1.3 hello-world.js

---
+++ week-1/hello-world.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Shelita Soriano
+; Date: June 2, 2018
+; Modified By: Shelita Soriano
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+
+console.log("Hello World, my name is Shelita Soriano");
